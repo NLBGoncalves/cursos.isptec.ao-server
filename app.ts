aa83d68c876a0a7cd96424949d0f2adfb0fde367
NLBGoncalves <nilsonlaerciobotelho123@gmail.com>
2024-03-01 15:06:19 +0100

    done

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["https://cursosisptecao.up.railway.app"],
+    origin: ["https://cursos.isptec.ao"],
     credentials: true,
   })
 );
