dd47dbf10d68c56b9a0581743b6f2a5e6f6f0e7f
NLBGoncalves <nilsonlaerciobotelho123@gmail.com>
2024-03-02 10:10:47 +0100

    done

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -22,7 +22,7 @@ userRouter.post("/activate-user", activateUser);
 
 userRouter.post("/login", loginUser);
 
-userRouter.get("/logout",isAutheticated, logoutUser);
+userRouter.get("/logout", isAutheticated, logoutUser);
 
 userRouter.get("/me", isAutheticated, getUserInfo);
 
